0032b0ad3ca581e28d70bb6d23103696fac17b74
AjayG9923 <ajaygore1999@gmail.com>
2024-09-29 12:57:26 +0530

    Updated asset link

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,17 +1,17 @@
-import {Dimensions, SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import WelcomeScreen from './WelcomeScreen';
-import {SceneMap, TabView, TabBar} from 'react-native-tab-view'; // Import TabBar
-import CategoriesScreen from './CategoriesScreen';
-import ImageComponent from '../components/ImageComponent';
+import { Dimensions, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import WelcomeScreen from "./WelcomeScreen";
+import { SceneMap, TabView, TabBar } from "react-native-tab-view"; // Import TabBar
+import CategoriesScreen from "./CategoriesScreen";
+import ImageComponent from "../components/ImageComponent";
 
 export default function Chat() {
-  const initialLayout = {width: Dimensions.get('window').width};
+  const initialLayout = { width: Dimensions.get("window").width };
 
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
-    {key: 'Welcome', title: 'Welcome'},
-    {key: 'Categories', title: 'Categories'},
+    { key: "Welcome", title: "Welcome" },
+    { key: "Categories", title: "Categories" },
   ]);
 
   const renderScene = SceneMap({
@@ -19,17 +19,18 @@ export default function Chat() {
     Categories: CategoriesScreen,
   });
 
-  const renderTabBar = props => (
+  const renderTabBar = (props) => (
     <TabBar
       {...props}
       style={styles.tabBar}
       indicatorStyle={styles.indicator}
-      renderLabel={({route, focused}) => (
+      renderLabel={({ route, focused }) => (
         <Text
           style={[
             styles.label,
             focused ? styles.activeLabel : styles.inactiveLabel,
-          ]}>
+          ]}
+        >
           {route.title}
         </Text>
       )}
@@ -37,23 +38,24 @@ export default function Chat() {
   );
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       <View
         style={{
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          flexDirection: 'row',
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexDirection: "row",
           marginHorizontal: 20,
           marginBottom: 20,
-        }}>
-        <View style={{flexDirection: 'row', gap: 20}}>
-          <ImageComponent source={require('../assets/Tab.png')} />
-          <ImageComponent source={require('../assets/color.png')} />
+        }}
+      >
+        <View style={{ flexDirection: "row", gap: 20 }}>
+          <ImageComponent source={require("../assets/tab.png")} />
+          <ImageComponent source={require("../assets/color.png")} />
         </View>
-        <ImageComponent source={require('../assets/bell.webp')} />
+        <ImageComponent source={require("../assets/bell.webp")} />
       </View>
       <TabView
-        navigationState={{index, routes}}
+        navigationState={{ index, routes }}
         renderScene={renderScene}
         renderTabBar={renderTabBar}
         onIndexChange={setIndex}
@@ -65,19 +67,19 @@ export default function Chat() {
 
 const styles = StyleSheet.create({
   tabBar: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   indicator: {
-    backgroundColor: 'pink',
+    backgroundColor: "pink",
   },
   label: {
     fontSize: 10,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   activeLabel: {
-    color: 'pink',
+    color: "pink",
   },
   inactiveLabel: {
-    color: 'black',
+    color: "black",
   },
 });
